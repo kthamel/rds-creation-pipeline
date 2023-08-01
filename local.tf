396c9129c3e7205119747404d36053cfa86b0bc1
Kushan Thamel <kushan.thamel@hotmail.com>
2023-08-02 00:01:25 +0530

    aws provider configuration was added

--- local.tf
+++ local.tf
@@ -1,4 +1,11 @@
 terraform {
+    required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  
   backend "s3" {
     bucket = "kthamel-s3-tfstate"
     key    = "kthamel-postgres"
